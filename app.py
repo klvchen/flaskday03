b18b65d41a4cb662b8c828bc1b2b0825b8bc063d
18664661957 <11911464@qq.com>
2020-08-25 20:45:22 +0800

    flask 向前端传递数据

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
@@ -8,10 +8,28 @@ def hello_world():
     return 'Hello World!'
 
 
-@app.route('/klvchen/')
-def klvchen():
-    return 'Hello klvchen'
+class Girl:
+    def __init__(self, name, addr):
+        self.name = name
+        self.gender = '女'
+        self.addr = addr
+
+    def __str__(self):
+        return self.name
+
+
+@app.route('/show')
+def show():
+    name = 'klvchen'
+    age = 18
+    friends = ['klvchen', 'lily', 'lucy', 'tom']
+    dict1 = {'gift': '大手镯', 'gift1': '鲜花', 'gift2': '费列罗'}
+
+    # 创建对象
+    girlfriend = Girl('lily', '广东')
+
+    return render_template('show.html', name=name, age=age, gender='男', friends=friends, dict1=dict1, girl=girlfriend )
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
